6ff64e38267ac3554ea389ce86d341ee431cab40
kuai-mare <orionkmc@gmail.com>
2023-04-26 09:20:46 -0500

    total con nc

--- apps/sales/views.py
+++ apps/sales/views.py
@@ -276,6 +276,8 @@ class MonthSaleSu(LoginRequiredMixin, View):
                 d_min = {'date__min': today}
             
             try:
+                print(round(total_valor_venta['sale_value__sum'], 2))
+                print(round(total_valor_nc['nc_value__sum']))
                 total_nc = round(total_valor_venta['sale_value__sum'], 2) - round(total_valor_nc['nc_value__sum'])
             except:
                 total_nc = 0
